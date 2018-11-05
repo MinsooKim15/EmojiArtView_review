1aec337c5cdf65d7888581ce79f3ede253e26911
minsookim <minsookim0615@gmail.com>
2018-11-05 22:53:36 +0900

    finished till the collectionView

---
+++ EmojiArtView_review/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArtView_review
+//
+//  Created by minsoo kim on 05/11/2018.
+//  Copyright © 2018 minsoo kim. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
